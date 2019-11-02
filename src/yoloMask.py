b8bbd9d8e793abf51d80c23fc9975d4760167de2
James Sohn <sohn21c@hotmail.com>
2019-11-02 17:04:05 -0500

    mask creation verified

--- src/yoloMask.py
+++ src/yoloMask.py
@@ -16,9 +16,6 @@ class generateMask(object):
     def __init__(self):
         self.path = os.getcwd()
 
-    def load_video(self, video_file):
-        return cv2.VideoCapture(video_file)
-
     def set_thresh(self, thresh):
         try:
             self.low = thresh[0]
@@ -29,6 +26,32 @@ class generateMask(object):
 
     def set_objName(self, obj_name):
         self.obj_name = obj_name
+
+    def load_video(self, video_file):
+        return cv2.VideoCapture(video_file)
+
+    def create_mask(self, frame):
+	# image processing
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     	thresh = cv2.threshold(gray, self.low, self.high, cv2.THRESH_BINARY)[1]
+        erode = cv2.erode(thresh, None, iterations=1)
+        cnts = cv2.findContours(erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = get_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+            
+        # mask generation
+        mask = np.zeros((frame.shape[0], frame.shape[1]))
+        cv2.drawContours(mask, [c], -1, (1,0,0), cv2.FILLED)
+            
+        # bounding box
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+	bbox_coord = (extLeft, extRight, extTop, extBot)
+	
+	return mask
 
 
 def main():
@@ -50,7 +73,7 @@ def main():
     
     # mode selection
     if args['mode'] == 'test':
-        gen_mask = generateMask()
+        gen_mask = generateMask(args['video'])
 
     elif args['mode'] == 'generate':
         pass
