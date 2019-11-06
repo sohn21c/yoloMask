8bfea65a2d646688f546848d3ff6476755fa123d
James Sohn <sohn21c@hotmail.com>
2019-11-05 21:50:36 -0600

    set_dest() created and tested

--- src/createDataset.py
+++ src/createDataset.py
@@ -17,6 +17,8 @@ class createDataset(object):
         pass
 
     def choose_mask(self, bboxFile):
+        """randomly chooses the object mask"""
+
         f = open(bboxFile, 'r')
         content = f.read().split('\n')
         ind = np.random.randint(len(content) - 1)
@@ -36,6 +38,8 @@ class createDataset(object):
         return maskFile, maskCoord
 
     def count_target(self):
+        """counts the number of objects for detection in MS-COCO dataset"""
+
         tally = {}
         for obj in self.target:
             tally[obj] = 0
@@ -57,18 +61,67 @@ class createDataset(object):
         print('[COUNT] done counting targets in dataset')
         print(tally)
 
+    def create(self):
+        """create dataset"""
+        new_image = 0   # counter
+
+        ind = 0 # for-loop counter
+        for ind in range(len(self.labelList)):
+            pass 
+            
+            
+            
+            
+            
+    def process_mask(maskFile, coordList, targetWidth, rot):
+        """crops the area outside bbox of mask and rotate"""
+        img = mpimg.imread(maskFile)
+
+        topx = int(coordList[0])
+        topy = int(coordList[1])
+        botx = int(coordList[2])
+        boty = int(coordList[3])
+
+        sliced = img[topy:boty, topx:botx, :]
+        rotated = util.rotate_bound(sliced, rot)
+        resized = util.resize_img(rotated, target_width)
+
+        return resized, resized.shape
+
     def load_labels(self, pathLabel):
+        """load labels as list"""
         self.pathLabel = pathLabel
         self.labelList = os.listdir(pathLabel)
  
+    def set_dest(self, dest, prefix):
+        """set up dir structure at destination folder"""
+        if not os.path.exists(dest):
+            os.mkdir(dest)
+        os.mkdir(dest+'/images')
+        os.mkdir(dest+'/images'+'/train')
+        os.mkdir(dest+'/images'+'/test')
+        os.mkdir(dest+'/labels')
+        os.mkdir(dest+'/labels'+'/train')
+        os.mkdir(dest+'/labels'+'/test')
+
+        self.prefix = prefix
+
     def set_target(self, target):
+        """convert target object index to be in consecutive order"""
+        # parse target objects
         res = []
         targets = target.split(',')
         for item in targets:
             res.append(item)
         self.target = res
     
+        # create conversion table for new index
+        self.conversion = {}
+        for i, cat in enumerate(self.target):
+            self.conversion[cat] = f'{i}'
+    
     def set_size(self, size):
+        """set number of custom object"""
         self.dtSize = size
        
 
@@ -88,20 +141,19 @@ def main():
     # mode assertion
     assert(args['mode'] in ['train', 'test', 'count']), 'Mode should be one of the followings: \'train\', \'test\', \'count\''
 
-    # parse configuration file
+    # parse configuration file and set constants
     info = util.parse_cfg(args['cfg'])
     for key, val in info.items():
-        #exec(key + '=val')
         globals()[key] = val
     
-    # body 
+    # main body
     mode = args['mode']
     if mode == 'train':
         c = createDataset()
         c.set_target(TARGET)
-        mask, coord = c.choose_mask(BBOX_TXT)
-        print(mask)
-        print(coord)
+        c.set_size(6000)
+        c.load_labels(TRAIN_LABEL)
+        c.set_dest(DET, 'blah')
 
     elif mode == 'test':
         pass
